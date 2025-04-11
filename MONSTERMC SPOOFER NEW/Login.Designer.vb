b271d9f888c1f457e5090762e87055c2910d60c2
Frustrate1888 <dfsuhsdfydfs7@outlook.com>
2025-04-11 14:03:00 +0400

    Login.Designer.vb

---
+++ MONSTERMC SPOOFER NEW/Login.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Guna2DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Guna2GradientPanel1 = New Guna.UI2.WinForms.Guna2GradientPanel()
+        Me.Guna2ControlBox1 = New Guna.UI2.WinForms.Guna2ControlBox()
+        Me.Guna2ImageButton4 = New Guna.UI2.WinForms.Guna2ImageButton()
+        Me.Guna2ImageButton3 = New Guna.UI2.WinForms.Guna2ImageButton()
+        Me.Guna2ImageButton2 = New Guna.UI2.WinForms.Guna2ImageButton()
+        Me.Guna2ImageButton1 = New Guna.UI2.WinForms.Guna2ImageButton()
+        Me.Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2GradientButton3SMBIOS = New Guna.UI2.WinForms.Guna2GradientButton()
+        Me.Guna2TextBox1 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.Guna2DragControl2 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.Guna2GradientPanel1.SuspendLayout()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Guna2Elipse1
+        '
+        Me.Guna2Elipse1.BorderRadius = 50
+        Me.Guna2Elipse1.TargetControl = Me
+        '
+        'Guna2DragControl1
+        '
+        Me.Guna2DragControl1.DockIndicatorTransparencyValue = 0.6R
+        Me.Guna2DragControl1.TargetControl = Me.Guna2GradientPanel1
+        Me.Guna2DragControl1.UseTransparentDrag = True
+        '
+        'Guna2GradientPanel1
+        '
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2ControlBox1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2ImageButton4)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2ImageButton3)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2ImageButton2)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2ImageButton1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2HtmlLabel1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2GradientButton3SMBIOS)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2TextBox1)
+        Me.Guna2GradientPanel1.Controls.Add(Me.Guna2PictureBox1)
+        Me.Guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Guna2GradientPanel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(36, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.Guna2GradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Guna2GradientPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2GradientPanel1.Name = "Guna2GradientPanel1"
+        Me.Guna2GradientPanel1.Size = New System.Drawing.Size(345, 295)
+        Me.Guna2GradientPanel1.TabIndex = 0
+        '
+        'Guna2ControlBox1
+        '
+        Me.Guna2ControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Guna2ControlBox1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ControlBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2ControlBox1.FillColor = System.Drawing.Color.Transparent
+        Me.Guna2ControlBox1.IconColor = System.Drawing.Color.White
+        Me.Guna2ControlBox1.Location = New System.Drawing.Point(305, 3)
+        Me.Guna2ControlBox1.Name = "Guna2ControlBox1"
+        Me.Guna2ControlBox1.Size = New System.Drawing.Size(29, 19)
+        Me.Guna2ControlBox1.TabIndex = 9
+        '
+        'Guna2ImageButton4
+        '
+        Me.Guna2ImageButton4.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ImageButton4.CheckedState.ImageSize = New System.Drawing.Size(64, 64)
+        Me.Guna2ImageButton4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2ImageButton4.HoverState.ImageSize = New System.Drawing.Size(28, 28)
+        Me.Guna2ImageButton4.Image = Global.MONSTERMC_SPOOFER_NEW.My.Resources.Resources.shopping_cart
+        Me.Guna2ImageButton4.ImageOffset = New System.Drawing.Point(0, 0)
+        Me.Guna2ImageButton4.ImageRotate = 0!
+        Me.Guna2ImageButton4.ImageSize = New System.Drawing.Size(30, 30)
+        Me.Guna2ImageButton4.Location = New System.Drawing.Point(106, 252)
+        Me.Guna2ImageButton4.Name = "Guna2ImageButton4"
+        Me.Guna2ImageButton4.PressedState.ImageSize = New System.Drawing.Size(28, 28)
+        Me.Guna2ImageButton4.Size = New System.Drawing.Size(39, 35)
+        Me.Guna2ImageButton4.TabIndex = 8
+        '
+        'Guna2ImageButton3
+        '
+        Me.Guna2ImageButton3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ImageButton3.CheckedState.ImageSize = New System.Drawing.Size(64, 64)
+        Me.Guna2ImageButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2ImageButton3.HoverState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton3.Image = Global.MONSTERMC_SPOOFER_NEW.My.Resources.Resources.DISCORD
+        Me.Guna2ImageButton3.ImageOffset = New System.Drawing.Point(0, 0)
+        Me.Guna2ImageButton3.ImageRotate = 0!
+        Me.Guna2ImageButton3.ImageSize = New System.Drawing.Size(35, 25)
+        Me.Guna2ImageButton3.Location = New System.Drawing.Point(194, 252)
+        Me.Guna2ImageButton3.Name = "Guna2ImageButton3"
+        Me.Guna2ImageButton3.PressedState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton3.Size = New System.Drawing.Size(39, 35)
+        Me.Guna2ImageButton3.TabIndex = 7
+        '
+        'Guna2ImageButton2
+        '
+        Me.Guna2ImageButton2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ImageButton2.CheckedState.ImageSize = New System.Drawing.Size(64, 64)
+        Me.Guna2ImageButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2ImageButton2.HoverState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton2.Image = Global.MONSTERMC_SPOOFER_NEW.My.Resources.Resources._437944_youtube_player_screen_technology_tv_icon
+        Me.Guna2ImageButton2.ImageOffset = New System.Drawing.Point(0, 0)
+        Me.Guna2ImageButton2.ImageRotate = 0!
+        Me.Guna2ImageButton2.ImageSize = New System.Drawing.Size(40, 40)
+        Me.Guna2ImageButton2.Location = New System.Drawing.Point(284, 242)
+        Me.Guna2ImageButton2.Name = "Guna2ImageButton2"
+        Me.Guna2ImageButton2.PressedState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton2.Size = New System.Drawing.Size(50, 42)
+        Me.Guna2ImageButton2.TabIndex = 6
+        '
+        'Guna2ImageButton1
+        '
+        Me.Guna2ImageButton1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ImageButton1.CheckedState.ImageSize = New System.Drawing.Size(64, 64)
+        Me.Guna2ImageButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2ImageButton1.HoverState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton1.Image = Global.MONSTERMC_SPOOFER_NEW.My.Resources.Resources._2635454_email_mail_social_telegram_icon
+        Me.Guna2ImageButton1.ImageOffset = New System.Drawing.Point(0, 0)
+        Me.Guna2ImageButton1.ImageRotate = 0!
+        Me.Guna2ImageButton1.ImageSize = New System.Drawing.Size(40, 40)
+        Me.Guna2ImageButton1.Location = New System.Drawing.Point(15, 245)
+        Me.Guna2ImageButton1.Name = "Guna2ImageButton1"
+        Me.Guna2ImageButton1.PressedState.ImageSize = New System.Drawing.Size(38, 38)
+        Me.Guna2ImageButton1.Size = New System.Drawing.Size(50, 42)
+        Me.Guna2ImageButton1.TabIndex = 5
+        '
+        'Guna2HtmlLabel1
+        '
+        Me.Guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.Guna2HtmlLabel1.Location = New System.Drawing.Point(86, 202)
+        Me.Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
+        Me.Guna2HtmlLabel1.Size = New System.Drawing.Size(12, 14)
+        Me.Guna2HtmlLabel1.TabIndex = 4
+        Me.Guna2HtmlLabel1.Text = ":::"
+        '
+        'Guna2GradientButton3SMBIOS
+        '
+        Me.Guna2GradientButton3SMBIOS.Animated = True
+        Me.Guna2GradientButton3SMBIOS.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2GradientButton3SMBIOS.BorderRadius = 8
+        Me.Guna2GradientButton3SMBIOS.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2GradientButton3SMBIOS.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2GradientButton3SMBIOS.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Guna2GradientButton3SMBIOS.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2GradientButton3SMBIOS.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Guna2GradientButton3SMBIOS.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Guna2GradientButton3SMBIOS.FillColor = System.Drawing.Color.FromArgb(CType(CType(16, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(23, Byte), Integer))
+        Me.Guna2GradientButton3SMBIOS.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(18, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(27, Byte), Integer))
+        Me.Guna2GradientButton3SMBIOS.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2GradientButton3SMBIOS.ForeColor = System.Drawing.Color.White
+        Me.Guna2GradientButton3SMBIOS.Location = New System.Drawing.Point(82, 220)
+        Me.Guna2GradientButton3SMBIOS.Name = "Guna2GradientButton3SMBIOS"
+        Me.Guna2GradientButton3SMBIOS.Size = New System.Drawing.Size(184, 26)
+        Me.Guna2GradientButton3SMBIOS.TabIndex = 3
+        Me.Guna2GradientButton3SMBIOS.Text = "Login"
+        '
+        'Guna2TextBox1
+        '
+        Me.Guna2TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Guna2TextBox1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(129, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.Guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Guna2TextBox1.DefaultText = ""
+        Me.Guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Guna2TextBox1.FillColor = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2TextBox1.Location = New System.Drawing.Point(13, 174)
+        Me.Guna2TextBox1.Name = "Guna2TextBox1"
+        Me.Guna2TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Guna2TextBox1.PlaceholderText = "Yor Key"
+        Me.Guna2TextBox1.SelectedText = ""
+        Me.Guna2TextBox1.Size = New System.Drawing.Size(321, 25)
+        Me.Guna2TextBox1.TabIndex = 1
+        Me.Guna2TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Guna2PictureBox1
+        '
+        Me.Guna2PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox1.FillColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox1.Image = CType(resources.GetObject("Guna2PictureBox1.Image"), System.Drawing.Image)
+        Me.Guna2PictureBox1.ImageRotate = 0!
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(2, 0)
+        Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(342, 164)
+        Me.Guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Guna2PictureBox1.TabIndex = 0
+        Me.Guna2PictureBox1.TabStop = False
+        '
+        'Guna2DragControl2
+        '
+        Me.Guna2DragControl2.DockIndicatorTransparencyValue = 0.6R
+        Me.Guna2DragControl2.TargetControl = Me.Guna2PictureBox1
+        Me.Guna2DragControl2.UseTransparentDrag = True
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 295)
+        Me.Controls.Add(Me.Guna2GradientPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.Guna2GradientPanel1.ResumeLayout(False)
+        Me.Guna2GradientPanel1.PerformLayout()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Guna2DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+    Friend WithEvents Guna2GradientPanel1 As Guna.UI2.WinForms.Guna2GradientPanel
+    Friend WithEvents Guna2TextBox1 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2PictureBox1 As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents Guna2GradientButton3SMBIOS As Guna.UI2.WinForms.Guna2GradientButton
+    Friend WithEvents Guna2ImageButton2 As Guna.UI2.WinForms.Guna2ImageButton
+    Friend WithEvents Guna2ImageButton1 As Guna.UI2.WinForms.Guna2ImageButton
+    Friend WithEvents Guna2HtmlLabel1 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2ImageButton4 As Guna.UI2.WinForms.Guna2ImageButton
+    Friend WithEvents Guna2ImageButton3 As Guna.UI2.WinForms.Guna2ImageButton
+    Friend WithEvents Guna2ControlBox1 As Guna.UI2.WinForms.Guna2ControlBox
+    Friend WithEvents Guna2DragControl2 As Guna.UI2.WinForms.Guna2DragControl
+End Class
